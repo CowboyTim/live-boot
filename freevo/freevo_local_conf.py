1c5103ef1ef8db6f51990aa0c5d93e3bd3d1e779
Tim Aerts <tim@oleeeh.(none)>
2009-08-07 21:56:36 +0200

    - first attempt on working freevo

---
+++ freevo/freevo_local_conf.py
@@ -0,0 +1,54 @@
+CONTROL_ALL_AUDIO   = 0
+MAJOR_AUDIO_CTRL    = 'PCM'
+MAX_VOLUME          = 100
+DEFAULT_VOLUME      = 60
+CONFIG_VERSION      = 5.24
+
+plugin.remove('mixer')
+plugin.remove('tv')
+plugin.remove('shutdown')
+plugin.remove('xine')
+plugin.remove('ossmixer')
+plugin.remove('image.apod')
+
+plugin.activate('usbstorage')
+plugin.activate('idlebar')
+plugin.activate('idlebar.clock',   level=50)
+
+
+START_FULLSCREEN_X  = 1
+
+EVENTS['video']['1'] = Event(VIDEO_SEND_MPLAYER_CMD, arg='switch_audio')
+EVENTS['video']['4'] = Event(VIDEO_SEND_MPLAYER_CMD, arg='sub_select')
+EVENTS['video']['7'] = Event(VIDEO_SEND_MPLAYER_CMD, arg='sub_remove')
+
+EVENTS['video']['2'] = Event(VIDEO_SEND_MPLAYER_CMD, arg='speed_incr +.10')
+EVENTS['video']['5'] = Event(VIDEO_SEND_MPLAYER_CMD, arg='speed_incr -.10')
+EVENTS['video']['0'] = Event(VIDEO_SEND_MPLAYER_CMD, arg='frame_step')
+
+EVENTS['video']['3'] = Event(VIDEO_SEND_MPLAYER_CMD, arg='panscan +.05')
+EVENTS['video']['6'] = Event(VIDEO_SEND_MPLAYER_CMD, arg='panscan -.05')
+
+EVENTS['video']['8'] = Event(VIDEO_SEND_MPLAYER_CMD, arg='switch_ratio 1.33334')
+EVENTS['video']['9'] = Event(VIDEO_SEND_MPLAYER_CMD, arg='switch_ratio 1.77778')
+
+MPLAYER_VERSION = 0.9
+MPLAYER_ARGS = { 'dvd'    : '-cache 8192 -monitoraspect 16:9 -af volnorm=2 -vf pp=fd',
+                 'vcd'    : '-cache 4096 -monitoraspect 16:9',
+                 'cd'     : '-cache 1024 -cdda speed=2',
+                 'mkv'    : '-cache 5000 -idx -alang jpn,ja -slang eng,en,En -framedrop -monitoraspect 16:9 -af volnorm=1',
+                 'ogm'    : '-cache 5000 -aid 1 -sid 0 -monitoraspect 16:9',
+                 'tv'     : '-nocache',
+                 'ivtv'   : '-cache 8192',
+                 'avi'    : '-cache 5000 -monitoraspect 16:9 -af volnorm=1',
+                 'mpg'    : '-cache 5000 -monitoraspect 16:9 -af volnorm=1 -vf pp=fd',
+                 'rm'     : '-cache 5000 -forceidx',
+                 'rmvb'   : '-cache 5000 -forceidx -monitoraspect 16:9',
+                 'webcam' : 'tv:// -tv driver=v4l:width=352:height=288:outfmt=yuy2:device=/dev/video2',
+                 'default': '-cache 5000 -monitoraspect 16:9 -af volnorm=1'
+                 }
+
+VIDEO_ITEMS = [ ('Video Archive', '/media/video') ]
+AUDIO_ITEMS = [ ('MP3 Collection', '/media/mp3') ]
+GAMES_ITEMS = [ ('Atari 2600', '/media/atari', ('GENERIC', '/usr/games/stella', '-zoom 4 -fullscreen -hidecursor -sound 1 -video soft', ' ', [ 'bin' ] )) ]
+
