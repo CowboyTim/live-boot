6cd19bfb35a813b8dff1b3b1096df8e781d51eae
Tim Aerts <aardbeiplantje@gmail.com>
2011-02-27 16:29:51 +0100

    - added first version of make_debian.sh: makes a debian squeeze with   debootstrap

---
+++ make_debian.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+
+tmp=/var/tmp
+datestr=`date +%s`
+tmpdir=$tmp/squeeze.$datestr
+
+debootstrap squeeze $tmpdir http://ftp.be.debian.org/debian
+
+mount proc $tmpdir/proc -t proc
+mount sysfs $tmpdir/sys -t sysfs
+
+cat >> $tmpdir/etc/apt/sources.list.d/ps3.extra.list <<Eol
+deb http://security.debian.org/ squeeze/updates main
+deb http://security.debian.org/ squeeze/updates main
+deb-src http://security.debian.org/ squeeze/updates main
+Eol
+chroot $tmpdir /bin/bash -c <<EOc
+dpkg -r yaboot powerpc-utils powerpc-ibm-utils
+aptitude update
+aptitude install \
+    openbox obconf  \
+    xserver-xorg-input-evdev xserver-xorg-input-mouse \
+    xserver-xorg-video-fbdev \
+    iceweasel python3 gcc-4.4-spu lib64gcc1 gcc-spu gcc-avr spu-gcc \
+    avr-libc bc bzip2  \
+    git zip p7zip rxvt-unicode screen sudo xpdf \
+    zlib1g-dev vim vim-runtime \
+    lsof ltrace strace \
+    colordiff wireless-tools wmctrl  uuid-runtime usbutils  \
+    wpasupplicant time p7zip-full socat \
+    nmap ntpdate lua5.1 lua5.1-doc lzma m4 make \
+    luarocks luasocket luasocket-dev liblua5.1-posix-dev  \
+    liblua5.1-posix1 liblua5.1-dev
+    tcpdump ttf-bitstream-vera xfonts-100dpi console-terminus \
+    xfonts-terminus xinit rpm perltidy  netcat llvm llvm-dev \
+    pwgen gtk2-engines-murrine gtk-chtheme luadoc numactl
+    fbpanel dnsutils device-tree-compiler deborphan debootstrap dzen2 ethtool \
+    evtest exiv2 ftp g++-4.4  gcc-4.4-multilib gccxml genisoimage git-svn \
+    curl dcraw ddclient dc bison flex autoconf automake bluez bluez-hcidump \
+    build-essential dhcp3-client fakechroot fakeroot perl-doc \
+    joystick oprofile mscompress cabextract xrestop squashfs-tools
+
+
+
+aptitude upgrade
+aptitude clean
+EOc
+
+fn=$tmp/root.squashfs.$datestr
+time nice -n 20 mksquashfs $tmpdir/* $fn -e $tmpdir/{proc,tmp,var/tmp,sys,mnt,media,home,dev,boot}
+
+echo
+echo $fn
