d70fe0b002b39e1f0d755f4650c6462343a6aefd
Tim Aerts <aardbeiplantje@gmail.com>
2011-02-28 23:39:47 +0100

    - minor cleanup/fix in make_debian.sh

--- make_debian.sh
+++ make_debian.sh
@@ -53,6 +53,8 @@ groupadd users -g 1000
 useradd tim -g users -s /bin/bash -m -u 1000 -G audio,fuse,adm,cdrom,sudo -f -1
 EOc
 
+mkdir -p $tmpdir/cgroup
+
 time nice -n 20 mksquashfs \
     $tmpdir/* \
     $fn.root \
