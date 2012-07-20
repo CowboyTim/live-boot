68177b5925cbd399bb11a1cf1938c44aab6f708d
CowboyTim <aardbeiplantje@gmail.com>
2012-07-20 23:50:08 +0200

    - install_packages.sh trimmerd a bit: added apt-get clean + autoclean

--- install_packages.sh
+++ install_packages.sh
@@ -51,15 +51,8 @@ console-setup	console-setup/store_defaults_in_debconf_db	boolean	true
 console-setup	console-setup/charmap47	select	UTF-8
 console-setup	console-setup/fontsize-fb47	select	16
 console-setup	console-setup/fontsize	string	16
-localepurge     localepurge/remove_no   note       
-localepurge     localepurge/verbose     boolean true
-localepurge     localepurge/dontbothernew       boolean false
-localepurge     localepurge/nopurge     multiselect     
-localepurge     localepurge/quickndirtycalc     boolean true
-localepurge     localepurge/mandelete   boolean true
-localepurge     localepurge/showfreedspace      boolean true
-localepurge     localepurge/none_selected       boolean true
 EOc
 
-#tasksel install desktop
 aptitude -y --allow-untrusted install desktop
+apt-get clean
+apt-get autoclean
