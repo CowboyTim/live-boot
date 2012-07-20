c2afd74d066584152d746e8b82a92618690fe734
CowboyTim <aardbeiplantje@gmail.com>
2012-07-20 23:48:54 +0200

    Revert "- install_packages.sh: remove my own debconf stuff"
    This reverts commit dd171f5383d63f79af0cba0995e05e780c4588cf.

--- install_packages.sh
+++ install_packages.sh
@@ -22,6 +22,44 @@ Eoe
 gpg --keyserver keyserver.ubuntu.com --recv-key ABF5BD827BD9BF62
 
 apt-get update
+debconf-set-selections <<EOc
+tzdata	tzdata/Zones/$timezone_area	select	$timezone_city
+tzdata	tzdata/Areas	select	$timezone_area
+keyboard-configuration	keyboard-configuration/ctrl_alt_bksp	boolean	true
+keyboard-configuration	keyboard-configuration/modelcode	string	pc105
+keyboard-configuration	keyboard-configuration/unsupported_layout	boolean	true
+keyboard-configuration	keyboard-configuration/unsupported_config_options	boolean	true
+keyboard-configuration	keyboard-configuration/variantcode	string
+keyboard-configuration	keyboard-configuration/unsupported_config_layout	boolean	true
+keyboard-configuration	keyboard-configuration/toggle	select	No toggling
+keyboard-configuration	keyboard-configuration/model	select	Generic 105-key (Intl) PC
+keyboard-configuration	keyboard-configuration/compose	select	No compose key
+keyboard-configuration	keyboard-configuration/layout	select
+keyboard-configuration	keyboard-configuration/xkb-keymap	select	us
+keyboard-configuration	keyboard-configuration/layoutcode	string	us
+keyboard-configuration	keyboard-configuration/variant	select	USA
+keyboard-configuration	keyboard-configuration/switch	select	No temporary switch
+keyboard-configuration	keyboard-configuration/unsupported_options	boolean	true
+keyboard-configuration	keyboard-configuration/store_defaults_in_debconf_db boolean	true
+keyboard-configuration	keyboard-configuration/altgr	select	The default for the keyboard layout
+keyboard-configuration	keyboard-configuration/optionscode	string	terminate:ctrl_alt_bksp
+console-setup	console-setup/codeset47	select	# Latin1 and Latin5 - western Europe and Turkic languages
+console-setup	console-setup/codesetcode	string	Lat15
+console-setup	console-setup/fontface47	select	Fixed
+console-setup	console-setup/fontsize-text47	select	16
+console-setup	console-setup/store_defaults_in_debconf_db	boolean	true
+console-setup	console-setup/charmap47	select	UTF-8
+console-setup	console-setup/fontsize-fb47	select	16
+console-setup	console-setup/fontsize	string	16
+localepurge     localepurge/remove_no   note       
+localepurge     localepurge/verbose     boolean true
+localepurge     localepurge/dontbothernew       boolean false
+localepurge     localepurge/nopurge     multiselect     
+localepurge     localepurge/quickndirtycalc     boolean true
+localepurge     localepurge/mandelete   boolean true
+localepurge     localepurge/showfreedspace      boolean true
+localepurge     localepurge/none_selected       boolean true
+EOc
+
+#tasksel install desktop
 aptitude -y --allow-untrusted install desktop
-apt-get clean
-apt-get autoclean
