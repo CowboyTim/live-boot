cd2c2d9350bfbd158dbba935812aedaaa3ff5d34
CowboyTim <aardbeiplantje@gmail.com>
2012-07-28 23:44:25 +0200

    - make_initrd_arm.sh: use the squashfs file as a source: can now be run as a   regular user.

--- make_initrd_arm.sh
+++ make_initrd_arm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sourcedirtmp="$1"
+squashfile="$1"
 kernelversion="$2"
 if [ -z $kernelversion ]; then
     kernelversion=$(uname -r)
@@ -17,48 +17,35 @@ export PATH=$PATH:/usr/sbin/:/sbin/
 trap exit ERR
 tmpdir=$(mktemp -d -p $tmpscratchdir initrd_XXXXXX)
 echo "tempdir to use $tmpdir"
-exec > >(tee $tmpdir/build.log)
-exec 2>&1
 mkdir -p $tmpdir
 
 echo "Making a new initramfs in $tmpdir"
 targetinitrd=$tmpscratchdir/initrd-$kernelversion.cpio
-# general stuff
-mkdir -p $tmpdir/{sbin,lib/udev/rules.d}
-(
-    cd $sourcedirtmp;
-    cp -a \
-        ./usr/lib/klibc/bin/* \
-        ./bin/busybox \
-        ./sbin/blkid \
-        ./sbin/udevd \
-        ./sbin/udevadm \
-        ./sbin/{lsmod,rmmod,modprobe} \
-        $tmpdir/sbin/
-    cp -a \
-        ./lib/libc.so.6 \
-        ./lib/ld-linux.so.3 \
-        ./lib/libselinux.so.1 \
-        ./lib/libm.so.6 \
-        ./lib/libdl.so.2 \
-        ./lib/libblkid.so.1 \
-        ./lib/libuuid.so.1 \
-        ./lib/libgcc_s.so.1 \
-        $tmpdir/lib/
-    cp -a \
-        ./lib/udev/{usb,scsi,path,edd,ata}_id \
-        $tmpdir/lib/udev/
-    cp -a \
-        ./lib/udev/rules.d/60-persistent-storage.rules \
-        ./lib/udev/rules.d/50-udev-default.rules \
-        ./lib/udev/rules.d/91-permissions.rules \
-        $tmpdir/lib/udev/rules.d/
-    ln -s ./sbin $tmpdir/bin
-)
+
+unsquashfs -i -f -d $tmpdir $squashfile \
+    /usr/lib/klibc/bin \
+    /bin/busybox \
+    /sbin/blkid \
+    /sbin/udevd \
+    /sbin/udevadm \
+    /sbin/{lsmod,rmmod,modprobe} \
+    /lib/libc.so.6 \
+    /lib/ld-linux.so.3 \
+    /lib/libselinux.so.1 \
+    /lib/libm.so.6 \
+    /lib/libdl.so.2 \
+    /lib/libblkid.so.1 \
+    /lib/libuuid.so.1 \
+    /lib/libgcc_s.so.1 \
+    /lib/udev/{usb,scsi,path,edd,ata}_id \
+    /lib/udev/rules.d/60-persistent-storage.rules \
+    /lib/udev/rules.d/50-udev-default.rules \
+    /lib/udev/rules.d/91-permissions.rules \
+    || exit 1
 #depmod  -b $tmpdir -a $kernelversion
 
 # my stuff + build
-cp $here/fastboot $tmpdir
+cp $here/fastboot $tmpdir/
 cp $here/fastboot_init $tmpdir/init
 (
     cd $tmpdir
