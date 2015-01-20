cf30b5b04f52ef0d6995b9b351e90ba62c11e6b5
CowboyTim <aardbeiplantje@gmail.com>
2015-01-20 22:24:55 +0100

    build_android_kernel.sh: more robust

--- build_android_kernel.sh
+++ build_android_kernel.sh
@@ -7,12 +7,6 @@ mkdir -p $tmpdir
 
 kernel_src_location="$1"
 root_fs_file="$2"
-if [ -z "$kernel_src_location" ]; then
-    kernel_src_location=~/GT-I9100_samsung
-fi
-if [ -z "$root_fs_file" ]; then
-    root_fs_file=~/data/squeeze.1355172793.squashfs.xz
-fi
 
 if [ ! -e "$root_fs_file" -o ! -d $kernel_src_location ]; then
     echo "usage: $0 [<kernel src>] [<root_fs_file>]"
@@ -21,10 +15,12 @@ fi
 
 export CROSS_COMPILE=/usr/bin/arm-linux-gnueabi-
 make_args="ARCH=arm INSTALL_MOD_PATH=$tmpdir -j10 "
-here=$(readlink -f -- "${0%/*}")
+here=$(readlink -f "${0%/*}")
+
+cd $kernel_src_location || exit 1
 
 for w in modules modules_install; do
-    make $make_args $w
+    make $make_args $w || exit 1
 done
 kernelversion=$(basename $(ls $tmpdir/lib/modules/))
 bash $here/make_initrd_arm.sh \
@@ -33,4 +29,3 @@ bash $here/make_initrd_arm.sh \
     && cp /var/tmp/initrd-$kernelversion.cpio.gz $kernel_src_location/initrd.cpio.gz
 
 make $make_args zImage 
-    
